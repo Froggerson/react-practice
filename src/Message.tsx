d5e6f58c7ab7871ee48ccffcb3a81fcdf2a28e0b
Froggerson <phamjennifer314@gmail.com>
2023-05-06 23:21:26 -0700

    Testing out function notation

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+//PascalCasing
+function Message() {
+    // JavaScript XML
+    const animal = 'cat';
+    if (animal)
+        return <h1>Hello {animal}.</h1>;
+    return <h1>Hello World!</h1>
+}
+
+export default Message;
